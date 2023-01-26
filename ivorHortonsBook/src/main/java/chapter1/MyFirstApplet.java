b24c9c195f96efed51c9e8edb40d940dbe84eee2
Doda <105274235+Doda0605@users.noreply.github.com>
2023-01-26 04:28:27 +0100

    Add files via upload

---
+++ ivorHortonsBook/src/main/java/chapter1/MyFirstApplet.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package chapter1;
+
+import javax.swing.JApplet;
+import java.awt.Graphics;
+/**
+ *
+ * @author David
+ */
+public class MyFirstApplet {
+    
+    public void paint(Graphics g){
+        g.drawString("To climb a ladder, start at the bottom rung.", 20, 90);
+    }
+    
+}
